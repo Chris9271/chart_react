76a1b30ce3039c50f5cca613e8f2df540a864fec
Chris9271 <chrisweng71@outlook.com>
2022-04-30 18:40:19 +0800

    add dependency

--- client/src/components/Spinner/Spinner.jsx
+++ client/src/components/Spinner/Spinner.jsx
@@ -4,7 +4,7 @@ import './Spinner.css';
 const Spinner = () => {
     return (
         <div className="spin">
-            <div className="load-msg">資料載入中...</div>
+            <div className="load-msg">資料載入中，請稍候...</div>
             <div className="loader"></div>
         </div>
     )
