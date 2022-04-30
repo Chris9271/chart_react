f362f1881480bf49f4355a241d0517e51d023b18
Chris9271 <chrisweng71@outlook.com>
2022-05-01 00:11:22 +0800

    update URL

--- client/src/components/utils/config.js
+++ client/src/components/utils/config.js
@@ -1,1 +1,1 @@
-export const API_URL = (process.env.BACKEND_API_URL || 'https://desolate-reaches-92998.herokuapp.com') + '/api';
+export const API_URL = (process.env.BACKEND_API_URL || 'http://localhost:5000') + '/api';
