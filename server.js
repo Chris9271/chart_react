d76f33356e056c64435c1501a61de25a542c132c
Chris9271 <chrisweng71@outlook.com>
2022-04-30 17:55:09 +0800

    chart_server-add-path

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const {createProxyMiddleware} = require('http-proxy-middleware');
 const app = express();
 
